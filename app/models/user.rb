95ebfbaf2a92e995d4c598543c5e786dc10dad4b
Jack Wu <xuwupeng2000@gmail.com>
2015-09-23 18:13:22 +1200

    Init devise and rolify

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ActiveRecord::Base
+  rolify
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
   has_many :customers
   has_many :product_users, :dependent => :destroy
   has_and_belongs_to_many :products, :through => :product_users
